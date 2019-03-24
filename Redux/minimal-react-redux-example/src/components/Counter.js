b2b8bb8916f575524f0c4c401cc17d1dbd26902d
Andrew Beveridge <andrew@beveridge.uk>
2019-03-24 21:39:06 +0000

    Updated Minimal React+Redux example to match Redux-only example, got tests passing, reformatted and tweaked code

--- Redux/minimal-react-redux-example/src/components/Counter.js
+++ Redux/minimal-react-redux-example/src/components/Counter.js
@@ -1,53 +1,58 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.incrementAsync = this.incrementAsync.bind(this);
-    this.incrementIfOdd = this.incrementIfOdd.bind(this);
-  }
-
-  incrementIfOdd() {
-    if (this.props.value % 2 !== 0) {
-      this.props.onIncrement()
+    constructor(props) {
+        super(props);
+        this.incrementIn3s = this.incrementIn3s.bind(this);
+        this.incrementIfOdd = this.incrementIfOdd.bind(this);
     }
-  }
 
-  incrementAsync() {
-    setTimeout(this.props.onIncrement, 1000)
-  }
+    incrementIfOdd() {
+        if (this.props.value % 2 !== 0) {
+            this.props.onIncrement()
+        }
+    }
 
-  render() {
-    const { value, onIncrement, onDecrement } = this.props
-    return (
-      <p>
-        Clicked: {value} times
-        {' '}
-        <button onClick={onIncrement}>
-          +
-        </button>
-        {' '}
-        <button onClick={onDecrement}>
-          -
-        </button>
-        {' '}
-        <button onClick={this.incrementIfOdd}>
-          Increment if odd
-        </button>
-        {' '}
-        <button onClick={this.incrementAsync}>
-          Increment async
-        </button>
-      </p>
-    )
-  }
+    incrementIn3s() {
+        setTimeout(this.props.onIncrement, 3000)
+    }
+
+    render() {
+        const {value, onIncrement, onDecrement} = this.props;
+        return (
+            <div>
+                <h1>Minimal React + Redux example</h1>
+                <p>
+                    Counter: {value} clicks
+                </p>
+                <p>
+                    <button onClick={onIncrement}>
+                        +
+                    </button>
+                    {' '}
+                    <button onClick={onDecrement}>
+                        -
+                    </button>
+                </p>
+                <p>
+                    <button onClick={this.incrementIfOdd}>
+                        Increment if odd
+                    </button>
+                    {' '}
+                    <button onClick={this.incrementIn3s}>
+                        Increment in 3s
+                    </button>
+                </p>
+            </div>
+        )
+    }
 }
 
 Counter.propTypes = {
-  value: PropTypes.number.isRequired,
-  onIncrement: PropTypes.func.isRequired,
-  onDecrement: PropTypes.func.isRequired
-}
+    value: PropTypes.number.isRequired,
+    onIncrement: PropTypes.func.isRequired,
+    onDecrement: PropTypes.func.isRequired
+};
 
 export default Counter
--- Redux/minimal-react-redux-example/src/index.js
+++ Redux/minimal-react-redux-example/src/index.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import {createStore} from 'redux'
 import Counter from './components/Counter'
 import counter from './reducers'
 
-const store = createStore(counter)
-const rootEl = document.getElementById('root')
+const store = createStore(counter);
+const rootDivElementForReactDOM = document.getElementById('root');
 
 const render = () => ReactDOM.render(
-  <Counter
-    value={store.getState()}
-    onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-    onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-  />,
-  rootEl
-)
+    <Counter
+        value={store.getState()}
+        onIncrement={() => store.dispatch({type: 'INCREMENT'})}
+        onDecrement={() => store.dispatch({type: 'DECREMENT'})}
+    />,
+    rootDivElementForReactDOM
+);
 
-render()
-store.subscribe(render)
+render();
+store.subscribe(render);
