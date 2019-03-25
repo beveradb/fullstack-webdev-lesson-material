9abe090b43b15440fa621c1767855100a4010e06
Andrew Beveridge <andrew@beveridge.uk>
2019-03-25 23:25:49 +0000

    Cleaned up code, added devtools extension

--- Redux/better-react-redux-example/src/containers/MainSection.js
+++ Redux/better-react-redux-example/src/containers/MainSection.js
@@ -1,23 +1,23 @@
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
 import * as TodoActions from '../actions'
-import { bindActionCreators } from 'redux'
+import {bindActionCreators} from 'redux'
 import MainSection from '../components/MainSection'
-import { getCompletedTodoCount } from '../selectors'
+import {getCompletedTodoCount} from '../selectors'
 
 
 const mapStateToProps = state => ({
-  todosCount: state.todos.length,
-  completedCount: getCompletedTodoCount(state)
-})
+    todosCount: state.todos.length,
+    completedCount: getCompletedTodoCount(state)
+});
 
 
 const mapDispatchToProps = dispatch => ({
-  actions: bindActionCreators(TodoActions, dispatch)
-})
+    actions: bindActionCreators(TodoActions, dispatch)
+});
 
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(MainSection)
 
--- Redux/better-react-redux-example/src/index.js
+++ Redux/better-react-redux-example/src/index.js
@@ -1,19 +1,17 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {createStore} from 'redux'
+import {devToolsEnhancer} from 'redux-devtools-extension'
 import {Provider} from 'react-redux'
 import App from './components/App'
 import reducer from './reducers'
 import 'todomvc-app-css/index.css'
 
-const store = createStore(
-    reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({trace: true})
-)
+const store = createStore(reducer, devToolsEnhancer({trace: true}));
 
 render(
     <Provider store={store}>
         <App/>
     </Provider>,
     document.getElementById('root')
-)
+);
